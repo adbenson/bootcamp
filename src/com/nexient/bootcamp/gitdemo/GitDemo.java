21fa28397bde84d8519ef664b099b52f1efd4d7f
Drew Benson <drew@adbenson.net>
2015-10-07 14:39:01 -0400

    Adding some "functionality"

--- src/com/nexient/bootcamp/gitdemo/GitDemo.java
+++ src/com/nexient/bootcamp/gitdemo/GitDemo.java
@@ -1,5 +1,24 @@
 package com.nexient.bootcamp.gitdemo;
 
+import javax.swing.JOptionPane;
+import javax.swing.JWindow;
+
 public class GitDemo {
 
+	public GitDemo() {
+		int[] array = {1, 4, 2, 2, 0};
+		
+		JWindow window = new JWindow();
+		JOptionPane.showMessageDialog(window, "Sum = " + sumArray(array));	
+	}
+	
+	public int sumArray(int[] array) {
+		int sum = 0;
+		
+		for (int val : array) {
+			sum = val;
+		}
+		
+		return sum;
+	}
 }
